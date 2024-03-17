2222ea0d1a2315a507ca8365d2340071f081bf4f
MMatiazzo <mathmatiazzo@gmail.com>
2024-03-17 18:52:00 -0300

    fix signin signup

--- src/infrastructure/framework/aws/lambda/auth-lambda.ts
+++ src/infrastructure/framework/aws/lambda/auth-lambda.ts
@@ -12,20 +12,21 @@ export class AuthLambda implements IAuthLambdaGateway {
   ) {
 
     console.log('PRINT NAS ENVS');
-    console.log('env.MY_AWS_REGION => ', `"${env.MY_AWS_REGION}"`);
-    console.log('env.AWS_LAMBDA_SDK_ACCESS_KEY => ', `"${env.AWS_LAMBDA_SDK_ACCESS_KEY}"`);
-    console.log('env.AWS_LAMBDA_SECRET_ACCESS_KEY => ', `"${env.AWS_LAMBDA_SECRET_ACCESS_KEY}"`);
+    console.log('env.MY_AWS_REGION => ', env.MY_AWS_REGION);
+    console.log('env.AWS_LAMBDA_SDK_ACCESS_KEY => ', env.AWS_LAMBDA_SDK_ACCESS_KEY);
+    console.log('env.AWS_LAMBDA_SECRET_ACCESS_KEY => ', env.AWS_LAMBDA_SECRET_ACCESS_KEY);
+    console.log('env.AWS_LAMBDA_FUNCTION_NAME => ', env.AWS_LAMBDA_FUNCTION_NAME);
 
     AWS.config.update({
-      region: `"${env.MY_AWS_REGION}"`,
-      accessKeyId: `"${env.AWS_LAMBDA_SDK_ACCESS_KEY}"`,
-      secretAccessKey: `"${env.AWS_LAMBDA_SECRET_ACCESS_KEY}"`
+      region: env.MY_AWS_REGION,
+      accessKeyId: env.AWS_LAMBDA_SDK_ACCESS_KEY,
+      secretAccessKey: env.AWS_LAMBDA_SECRET_ACCESS_KEY
     });
 
     const lambda = new AWS.Lambda();
 
     const params = {
-      FunctionName: `"${env.AWS_LAMBDA_FUNCTION_NAME}"`,
+      FunctionName: env.AWS_LAMBDA_FUNCTION_NAME,
       Payload: JSON.stringify({ cpf, password, signup, username, email })
     };
 
